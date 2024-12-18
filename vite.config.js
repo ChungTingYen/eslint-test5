0b2b76316fd45991c53d7864979153a75c611865
AllanYen <iamallan0917@gmail.com>
2024-12-18 09:59:05 +0800

    3

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base:process.env.NODE_env==='production'?'/eslint-test5/':''
+  base:process.env.NODE_ENV==='production'?'/eslint-test5/':''
 });
